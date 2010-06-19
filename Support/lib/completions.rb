c607de58dccc557b0cf20c9ce4e54923b15d9674
Martin Kühl <martin.kuehl@gmail.com>
2010-06-19 05:30:18 +0200

    Use TextMate's `save_current_document` feature.
    * Let TextMate handle unsaved changes instead of the user.
    * Change the command's save and input options to enable the feature.
    * Pass the generated file path to the `RSenseCompletions` instance.

--- Support/lib/completions.rb
+++ Support/lib/completions.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 
+require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/save_current_document"
 require "#{ENV['TM_SUPPORT_PATH']}/lib/ui"
 
 require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/rsense_completions"
 
+TextMate.save_current_document
+
 rsense = RSenseCompletions.new
 
-choices = rsense.completion_list.map{|m| {"display" => "#{m[0]}"}}
 options = {:case_insensitive => false, :initial_filter => rsense.prefix}
+choices = rsense.completion_list(ENV['TM_FILEPATH']).collect do |comp|
+  {"display" => comp.first}
+end
 
 TextMate::UI.complete(choices, options)
